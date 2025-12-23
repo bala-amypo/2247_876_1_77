95ff24592f22a91c1ff521287023af5a2aded94a
bala-amypo <bala@amypo.in>
2025-12-23 09:55:02 +0000

    User commit

--- src/main/java/com/example/demo/repository/SkillRepository.java
+++ src/main/java/com/example/demo/repository/SkillRepository.java
@@ -3,9 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.entity.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface SkillRepository extends JpaRepository<Skill, Long> {
-
-    Optional<Skill> findBySkillName(String skillName);
 }
