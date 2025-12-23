30d21005797d81730580efd331bafcc744dd0ce9
bala-amypo <bala@amypo.in>
2025-12-23 18:10:07 +0000

    User commit

--- src/main/java/com/example/demo/entity/AssessmentResult.java
+++ src/main/java/com/example/demo/entity/AssessmentResult.java
@@ -1,11 +1,14 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AssessmentResult {
 
     @Id
@@ -16,60 +19,4 @@ public class AssessmentResult {
     private Long skillId;
     private double score;
     private String assessedAt;
-
-    /* ---------- Constructors ---------- */
-
-    public AssessmentResult() {
-    }
-
-    public AssessmentResult(Long id, Long studentProfileId, Long skillId,
-                            double score, String assessedAt) {
-        this.id = id;
-        this.studentProfileId = studentProfileId;
-        this.skillId = skillId;
-        this.score = score;
-        this.assessedAt = assessedAt;
-    }
-
-    /* ---------- Getters & Setters ---------- */
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getStudentProfileId() {
-        return studentProfileId;
-    }
-
-    public void setStudentProfileId(Long studentProfileId) {
-        this.studentProfileId = studentProfileId;
-    }
-
-    public Long getSkillId() {
-        return skillId;
-    }
-
-    public void setSkillId(Long skillId) {
-        this.skillId = skillId;
-    }
-
-    public double getScore() {
-        return score;
-    }
-
-    public void setScore(double score) {
-        this.score = score;
-    }
-
-    public String getAssessedAt() {
-        return assessedAt;
-    }
-
-    public void setAssessedAt(String assessedAt) {
-        this.assessedAt = assessedAt;
-    }
 }
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,11 +1,14 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -16,60 +19,4 @@ public class User {
     private String email;
     private String password;
     private Role role;
-
-    /* ---------- Constructors ---------- */
-
-    public User() {
-    }
-
-    public User(Long id, String fullName, String email,
-                String password, Role role) {
-        this.id = id;
-        this.fullName = fullName;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-
-    /* ---------- Getters & Setters ---------- */
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
 }
