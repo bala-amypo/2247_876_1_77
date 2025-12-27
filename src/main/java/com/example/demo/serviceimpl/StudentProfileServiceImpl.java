ce71d5562f28637275f95203e77f471696d4767b
bala-amypo <bala@amypo.in>
2025-12-27 18:23:24 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/StudentProfileServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/StudentProfileServiceImpl.java
@@ -1,11 +1,47 @@
+// package com.example.demo.serviceimpl;
+
+// import com.example.demo.entity.StudentProfile;
+// import com.example.demo.repository.StudentProfileRepository;
+// import java.util.List;
+// @Service
+// public class StudentProfileServiceImpl implements StudentProfileService {
+// {
+
+//     private final StudentProfileRepository repo;
+
+//     public StudentProfileServiceImpl(StudentProfileRepository repo) {
+//         this.repo = repo;
+//     }
+
+//     public StudentProfile createOrUpdateProfile(StudentProfile profile) {
+//         return repo.save(profile);
+//     }
+
+//     public StudentProfile getByUserId(Long userId) {
+//         return repo.findByUserId(userId)
+//                 .orElseThrow(() -> new RuntimeException("profile not found"));
+//     }
+
+//     public StudentProfile getProfileById(Long id) {
+//         return repo.findById(id)
+//                 .orElseThrow(() -> new RuntimeException("profile not found"));
+//     }
+
+//     public List<StudentProfile> getAllProfiles() {
+//         return repo.findAll();
+//     }
+// }
 package com.example.demo.serviceimpl;
 
 import com.example.demo.entity.StudentProfile;
 import com.example.demo.repository.StudentProfileRepository;
+import com.example.demo.service.StudentProfileService;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
+
 @Service
 public class StudentProfileServiceImpl implements StudentProfileService {
-{
 
     private final StudentProfileRepository repo;
 
@@ -13,20 +49,24 @@ public class StudentProfileServiceImpl implements StudentProfileService {
         this.repo = repo;
     }
 
+    @Override
     public StudentProfile createOrUpdateProfile(StudentProfile profile) {
         return repo.save(profile);
     }
 
+    @Override
     public StudentProfile getByUserId(Long userId) {
         return repo.findByUserId(userId)
                 .orElseThrow(() -> new RuntimeException("profile not found"));
     }
 
+    @Override
     public StudentProfile getProfileById(Long id) {
         return repo.findById(id)
                 .orElseThrow(() -> new RuntimeException("profile not found"));
     }
 
+    @Override
     public List<StudentProfile> getAllProfiles() {
         return repo.findAll();
     }
