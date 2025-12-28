b22458b52bfb673231477fd73496c80ffdc82723
bala-amypo <bala@amypo.in>
2025-12-28 03:52:11 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -16,32 +16,32 @@
 //         return "OK";
 //     }
 // }
-package com.example.demo.controller;
+// package com.example.demo.controller;  2
 
-import com.example.demo.entity.User;
-import com.example.demo.service.UserService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+// import com.example.demo.entity.User;
+// import com.example.demo.service.UserService;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+// import java.util.List;
 
-@RestController
-@RequestMapping("/users")
-public class UserController {
+// @RestController
+// @RequestMapping("/users")
+// public class UserController {
 
-    private final UserService userService;
+//     private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
-        return ResponseEntity.ok(userService.getById(id));
-    }
+//     @GetMapping("/{id}")
+//     public ResponseEntity<User> getUser(@PathVariable Long id) {
+//         return ResponseEntity.ok(userService.getById(id));
+//     }
 
-    @GetMapping("/instructors")
-    public ResponseEntity<List<User>> instructors() {
-        return ResponseEntity.ok(userService.listInstructors());
-    }
-}
+//     @GetMapping("/instructors")
+//     public ResponseEntity<List<User>> instructors() {
+//         return ResponseEntity.ok(userService.listInstructors());
+//     }
+// }
