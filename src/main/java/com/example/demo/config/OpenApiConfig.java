19b09f9fa4fa9a085a50abc402edecff07eb0554
bala-amypo <bala@amypo.in>
2025-12-28 03:37:11 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,14 +1,34 @@
 
+// package com.example.demo.config;
+
+// import io.swagger.v3.oas.models.OpenAPI;
+// import org.springframework.context.annotation.*;
+
+// @Configuration
+// public class OpenApiConfig {
+
+//     @Bean
+//     public OpenAPI openAPI() {
+//         return new OpenAPI();
+//     }
+// }
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import org.springframework.context.annotation.*;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI();
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://9137.32procr.amypo.ai")
+                ));
     }
 }
--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -35,23 +35,27 @@
 //                 .servers(List.of(server));
 //     }
 // }
-package com.example.demo.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 
-@Configuration
-public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .servers(List.of(
-                        new Server().url("https://9137.32procr.amypo.ai")
-                ));
-    }
-}
+// package com.example.demo.config;
+
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.servers.Server;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+
+// import java.util.List;
+
+// @Configuration
+// public class SwaggerConfig {
+
+//     @Bean
+//     public OpenAPI openAPI() {
+//         return new OpenAPI()
+//                 .servers(List.of(
+//                         new Server().url("https://9137.32procr.amypo.ai")
+//                 ));
+//     }
+// }
