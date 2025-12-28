c90089e9d5b12a4563ff05527097647f1413efb7
bala-amypo <bala@amypo.in>
2025-12-28 18:04:18 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
@@ -1,4 +1,35 @@
 
+// package com.example.demo.config;
+
+// import jakarta.servlet.*;
+// import jakarta.servlet.http.HttpServletRequest;
+// import java.io.IOException;
+
+// public class JwtAuthenticationFilter implements Filter {
+//     @Override
+//     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//             throws IOException, ServletException {
+//         chain.doFilter(request, response);
+//     }
+// }
+
+
+
+// package com.example.demo.config;
+
+// import jakarta.servlet.*;
+// import jakarta.servlet.http.HttpServletRequest;
+// import java.io.IOException;
+
+// public class JwtAuthenticationFilter implements Filter {
+//     @Override
+//     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//             throws IOException, ServletException {
+//         chain.doFilter(request, response);
+//     }
+// }
+
+
 package com.example.demo.config;
 
 import jakarta.servlet.*;
